7bc57e9735d8fbe0934b29e4b3d7baf9ecfbff7c
tcode <tk@tcode.dev>
2024-11-17 08:47:23 +0900

    画像のサイズ指定

--- components/molecules/ArtworkImage/ArtworkImagePresenter.tsx
+++ components/molecules/ArtworkImage/ArtworkImagePresenter.tsx
@@ -5,7 +5,7 @@ type Props = ImageProps & {
 };
 
 const ArtworkImagePresenter: React.FC<Props> = ({ data, ...props }) => {
-  return <Image source={{ uri: `data:image/png;base64,${data}` }} {...props} />;
+  return <Image source={{ uri: `data:image/png;base64,${data}` }} style={{width: props.width, height: props.height}} {...props} />;
 };
 
 export default ArtworkImagePresenter;

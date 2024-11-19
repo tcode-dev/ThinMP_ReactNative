11297c7893902822f850de4d50fd508ced17b18a
tcode <tk@tcode.dev>
2024-11-20 07:44:34 +0900

    AlbumDetailのCollapsingToolbar

--- components/templates/AlbumDetailTemplate/AlbumDetailTemplatePresenter.tsx
+++ components/templates/AlbumDetailTemplate/AlbumDetailTemplatePresenter.tsx
@@ -1,64 +1,37 @@
-import { Animated, View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { AlbumProps } from 'audio';
 import PageContainer from '@/components/atoms/PageContainer';
-import ArtworkImage from '@/components/molecules/ArtworkImage';
-import PrimaryTitle from '@/components/atoms/Title/PrimaryTitle';
-import SecondaryTitle from '@/components/atoms/Title/SecondaryTitle';
-import StickyHeader, { Props as StickyHeaderProps } from '@/components/organisms/StickyHeader';
-import StickyTitle from '@/components/organisms/StickyTitle';
+import CollapsingToolbar from '@/components/molecules/CollapsingToolbar';
 import SongList from '@/components/organisms/SongList';
-
-export const TITLE_BOTTOM_POSITION = 50;
+import ArtworkImage from '@/components/molecules/ArtworkImage';
 
 type Props = {
   albumDetail: AlbumProps;
   size: number;
-  titleHeight: number;
-  scrollY: Animated.Value;
-} & Pick<StickyHeaderProps, 'endPoint'>;
+};
 
-const AlbumDetailPresenter: React.FC<Props> = ({ albumDetail, size, titleHeight, scrollY, endPoint }) => {
+const AlbumDetailPresenter: React.FC<Props> = ({ albumDetail, size }) => {
   return (
     <PageContainer>
-      <ScrollView style={styles.container} onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], { useNativeDriver: false })} scrollEventThrottle={100}>
-        <StickyHeader title={albumDetail.name} scrollY={scrollY} endPoint={endPoint} />
-        <View style={styles.firstView}>
-          <ArtworkImage imageId={albumDetail.imageId} width={size} height={size} />
-          <LinearGradient colors={['transparent', '#ffffff']} style={[styles.linearGradient, { height: size * 0.5 }]} />
-          <StickyTitle scrollY={scrollY} endPoint={endPoint}>
-            <PrimaryTitle style={[styles.title, { height: titleHeight, lineHeight: titleHeight }]}>{albumDetail.name}</PrimaryTitle>
-          </StickyTitle>
-          <SecondaryTitle style={styles.description}>{albumDetail.artistName}</SecondaryTitle>
-        </View>
-        <SongList scrollEnabled={false} />
-      </ScrollView>
+      <CollapsingToolbar
+        title={albumDetail.name}
+        description={albumDetail.artistName}
+        components={[<SongList scrollEnabled={false} />]}
+        background={
+          <View style={styles.container}>
+            <ArtworkImage imageId={albumDetail.imageId} width={size} height={size} />
+            <LinearGradient colors={['transparent', '#ffffff']} style={[styles.linearGradient, { height: size * 0.5 }]} />
+          </View>
+        }
+      />
     </PageContainer>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  firstView: {
     position: 'relative',
-  },
-  title: {
-    position: 'absolute',
-    right: 0,
-    bottom: TITLE_BOTTOM_POSITION,
-    left: 0,
-    textAlign: 'center',
-    verticalAlign: 'middle',
-  },
-  description: {
-    position: 'absolute',
-    right: 0,
-    bottom: 25,
-    left: 0,
-    textAlign: 'center',
   },
   linearGradient: {
     position: 'absolute',

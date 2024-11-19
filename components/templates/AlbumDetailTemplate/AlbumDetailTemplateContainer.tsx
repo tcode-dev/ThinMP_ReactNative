a02ad8e24e97e26f83e482503aed691849337189
tcode <tk@tcode.dev>
2024-11-20 08:42:02 +0900

    CollapsingToolbar

--- components/templates/AlbumDetailTemplate/AlbumDetailTemplateContainer.tsx
+++ components/templates/AlbumDetailTemplate/AlbumDetailTemplateContainer.tsx
@@ -1,20 +1,15 @@
 import { Dimensions } from 'react-native';
-import { useScrollY } from '@/hooks/useScrollY';
 import useAlbumDetailStore from '@/store/albumDetailStore';
-import { useStickyHeaderEndPoint } from '@/hooks/useStickyHeaderEndPoint';
-import { useHeaderTitleHeight } from '@/hooks/useHeaderTitleHeight';
-import AlbumDetailPresenter, { TITLE_BOTTOM_POSITION } from './AlbumDetailTemplatePresenter';
+import AlbumDetailPresenter from './AlbumDetailTemplatePresenter';
 
 const AlbumDetailContainer = () => {
   const { state: albumDetailState } = useAlbumDetailStore();
-  const width = Dimensions.get('window').width;
-  const titleHeight = useHeaderTitleHeight();
-  const scrollY = useScrollY();
-  const endPoint = useStickyHeaderEndPoint(TITLE_BOTTOM_POSITION);
 
   if (!albumDetailState.isReady) return null;
 
-  return <AlbumDetailPresenter albumDetail={albumDetailState.value} size={width} titleHeight={titleHeight} scrollY={scrollY} endPoint={endPoint} />;
+  const width = Dimensions.get('window').width;
+
+  return <AlbumDetailPresenter albumDetail={albumDetailState.value} size={width} />;
 };
 
 export default AlbumDetailContainer;

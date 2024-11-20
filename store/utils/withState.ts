7e7dcf2344924ad315bff3cdc3c0e040fdde359d
tcode <tk@tcode.dev>
2024-11-21 07:52:26 +0900

    setStateの共通化

---
+++ store/utils/withState.ts
@@ -0,0 +1,15 @@
+import { Result, toSuccess, toFailure } from '@/type/Result';
+
+type AsyncFunction<T> = () => Promise<T>;
+
+const withState = async <T>(asyncFunction: AsyncFunction<T>, setState: (state: Result<T>) => void): Promise<void> => {
+  try {
+    const result = await asyncFunction();
+
+    setState(toSuccess(result));
+  } catch (error) {
+    setState(toFailure());
+  }
+};
+
+export default withState;

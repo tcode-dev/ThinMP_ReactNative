21eb0377cef39828ef83b8b012d462beac48401b
tcode <tk@tcode.dev>
2024-11-23 08:23:06 +0900

    EmptyHeader

---
+++ components/atoms/Header/EmptyHeader.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from 'react-native';
+import { getHeaderHeight } from '@/constants/Style';
+
+const EmptyHeader: React.FC = () => {
+  return <View style={styles.container} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: getHeaderHeight() + 20,
+  },
+});
+
+export default EmptyHeader;
---
+++ components/atoms/Header/index.tsx
@@ -0,0 +1,1 @@
+export { default as EmptyHeader } from './EmptyHeader';

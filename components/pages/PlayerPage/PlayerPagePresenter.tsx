288d0dde7e4617fccaf8a58dd563fa5a9efb859b
tcode <tk@tcode.dev>
2024-12-08 18:41:13 +0900

    player pageのタイトル

--- components/pages/PlayerPage/PlayerPagePresenter.tsx
+++ components/pages/PlayerPage/PlayerPagePresenter.tsx
@@ -3,6 +3,10 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { SongProps } from '@/packages/audio/build';
 import PageContainer from '@/components/atoms/PageContainer';
 import ArtworkImage from '@/components/molecules/ArtworkImage';
+import { PrimaryTitle, SecondaryTitle } from '@/components/atoms/Title';
+import { Style } from '@/constants/Style';
+
+export const TITLE_BOTTOM_POSITION = 50;
 
 export type Props = {
   bottom: number;
@@ -18,6 +22,8 @@ const PlayerPagePresenter: React.FC<Props> = ({ name, artistName, imageId, size,
         <View style={styles.artistImage}>
           <ArtworkImage imageId={imageId} width={size / 3} height={size / 3} />
         </View>
+        <PrimaryTitle style={[styles.title, { height: Style.headerTitleHeight, lineHeight: Style.headerTitleHeight }]}>{name}</PrimaryTitle>
+        <SecondaryTitle style={styles.description}>{artistName}</SecondaryTitle>
       </View>
     </PageContainer>
   );
@@ -42,6 +48,21 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
+  },
+  title: {
+    position: 'absolute',
+    right: 0,
+    bottom: TITLE_BOTTOM_POSITION,
+    left: 0,
+    textAlign: 'center',
+    verticalAlign: 'middle',
+  },
+  description: {
+    position: 'absolute',
+    right: 0,
+    bottom: 25,
+    left: 0,
+    textAlign: 'center',
   },
 });
 

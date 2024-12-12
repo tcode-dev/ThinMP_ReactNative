138eb6c382d904fb6943aa0822589368917b01cb
tcode <tk@tcode.dev>
2024-12-13 08:24:50 +0900

    PlaybackButtonが表示されない場合があったのを修正

--- components/organisms/MiniPlayer/MiniPlayerContainer.tsx
+++ components/organisms/MiniPlayer/MiniPlayerContainer.tsx
@@ -1,12 +1,15 @@
-import usePlaybackStore from '@/store/playbackStore';
-import MiniPlayerPresenter from './MiniPlayerPresenter';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import usePlaybackStore from '@/store/playbackStore';
+import useIsPlayingStore from '@/store/isPlayingStore';
+import MiniPlayerPresenter from './MiniPlayerPresenter';
 
 const MiniPlayerContainer = () => {
   const insets = useSafeAreaInsets();
   const { state: playbackState } = usePlaybackStore();
+  const { state: isPlayingState } = useIsPlayingStore();
 
   if (!playbackState.isReady) return null;
+  if (!isPlayingState.isReady) return null;
 
   const href = '/player';
 

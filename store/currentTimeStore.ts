27cad146fcd7f31ea6eb41482e4f6dc86bf79087
tcode <tk@tcode.dev>
2024-12-15 11:27:39 +0900

    曲停止、ページ遷移でSeekBarの更新を停止する

--- store/currentTimeStore.ts
+++ store/currentTimeStore.ts
@@ -1,33 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { atom, useAtom } from 'jotai';
 import Audio, { CurrentTimeProps } from 'audio';
 import { Result, toLoading } from '@/type/Result';
 import withState from './utils/withState';
+import useIsPlayingStore from './isPlayingStore';
 
 const currentTimeAtom = atom<Result<CurrentTimeProps>>(toLoading());
 const INTERVAL_MS = 1000;
 
 const useCurrentTimeStore = () => {
   const [state, setState] = useAtom(currentTimeAtom);
-
+  const { state: isPlayingState } = useIsPlayingStore();
+  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null);
   const getCurrentTime = async (): Promise<void> => {
     await withState<CurrentTimeProps>(() => Audio.getCurrentTime(), setState);
   };
-
   const resetCurrentTime = () => {
     setState(toLoading());
   };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      getCurrentTime();
-    }, INTERVAL_MS);
+    const updateCurrentTime = async () => {
+      if (isPlayingState.isReady && isPlayingState.value.isPlaying) {
+        await getCurrentTime();
+        timeoutIdRef.current = setTimeout(updateCurrentTime, INTERVAL_MS);
+      }
+    };
+    const cleanup = () => {
+      if (timeoutIdRef.current) {
+        clearTimeout(timeoutIdRef.current);
+        timeoutIdRef.current = null;
+      }
+    }
+
+    updateCurrentTime();
+
+    if (!isPlayingState.isReady || !isPlayingState.value.isPlaying) {
+      cleanup();
+    }
 
     return () => {
-      clearInterval(interval);
-      resetCurrentTime();
+      cleanup();
     };
-  }, []);
+  }, [isPlayingState]);
 
   return { state };
 };

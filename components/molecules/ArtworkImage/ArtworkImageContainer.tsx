133f8017a8438ecc2f635b475d97aa8913f85fdd
tcode <tk@tcode.dev>
2024-12-29 08:21:38 +0900

    Implement artwork caching and task queue for loading images in ArtworkImageContainer

--- components/molecules/ArtworkImage/ArtworkImageContainer.tsx
+++ components/molecules/ArtworkImage/ArtworkImageContainer.tsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from 'react';
 import { ImageProps, View } from 'react-native';
 import { Result, toLoading, toSuccess, toFailure } from '@/type/Result';
-import Audio, { SongProps } from 'audio';
+import { SongProps } from 'audio';
+import { taskQueue } from '@/utils/queue';
+import useArtworkStore from '@/store/artworkStore';
 import ArtworkImagePresenter from './ArtworkImagePresenter';
 import PlaceholderPresenter from './PlaceholderPresenter';
 
 type Props = ImageProps & Pick<SongProps, 'imageId'>;
 
 const ArtworkImageContainer: React.FC<Props> = ({ imageId, ...props }) => {
+  const {getArtwork} = useArtworkStore();
   const [state, setState] = useState<Result<string>>(toLoading());
 
   useEffect(() => {
     const load = async () => {
       try {
-        const result = await Audio.getArtwork(imageId);
-        if (result) {
-          setState(toSuccess(result));
+        const artwork = await getArtwork(imageId);
+
+        if (artwork) {
+          setState(toSuccess(artwork));
         } else {
           setState(toFailure());
         }
@@ -24,7 +28,7 @@ const ArtworkImageContainer: React.FC<Props> = ({ imageId, ...props }) => {
       }
     };
 
-    load();
+    taskQueue.add(load);
   }, [imageId]);
 
   if (state.isLoading) return <View style={{ width: props.width, height: props.height }} />;
---
+++ store/artworkStore.ts
@@ -0,0 +1,31 @@
+import { atom, useAtom } from 'jotai';
+import Audio from 'audio';
+
+type Props = Map<string, string | null>;
+
+const artworkAtom = atom<Props>(new Map());
+
+const useArtworkStore = () => {
+  const [state, setState] = useAtom(artworkAtom);
+  const getArtwork = async (id: string): Promise<string | null> => {
+    const cache = state.get(id);
+
+    if (cache !== undefined) {
+      return cache;
+    }
+
+    const result = await Audio.getArtwork(id);
+
+    setState(new Map(state.set(id, result)));
+
+    return result;
+  };
+
+  const resetArtwork = () => {
+    setState(new Map());
+  };
+
+  return { getArtwork, resetArtwork };
+};
+
+export default useArtworkStore;

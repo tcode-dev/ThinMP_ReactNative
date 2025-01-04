f406f5e8e42489e73654910bc767326959b8db1b
tcode <tk@tcode.dev>
2025-01-04 09:26:55 +0900

    refactor: remove unused Log import from AudioModule

--- packages/audio/android/src/main/java/dev/tcode/thinmpr/audio/AudioModule.kt
+++ packages/audio/android/src/main/java/dev/tcode/thinmpr/audio/AudioModule.kt
@@ -25,7 +25,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import android.util.Log
 
 class AudioModule : Module() {
   private val context

876d2bc58582ec95b649477607d4a1a0b266a7c8
tcode <tk@tcode.dev>
2025-01-11 09:25:07 +0900

    feat: refactor checkAndRequestPermissions to use useCallback for improved performance

--- store/permissionStore.ts
+++ store/permissionStore.ts
@@ -8,14 +8,14 @@ const permissionAtom = atom<Result<boolean>>(toLoading());
 
 export const usePermissionStore = () => {
   const [state, setState] = useAtom(permissionAtom);
-  const checkAndRequestPermissions = async (): Promise<boolean> => {
+  const checkAndRequestPermissions = useCallback(async (): Promise<boolean> => {
     const checked = await checkPermission();
     if (checked) {
       return checked;
     } else {
       return requestPermission();
     }
-  };
+  }, []);
   const ensurePermissions = useCallback(async (): Promise<void> => {
     await withState<boolean>(checkAndRequestPermissions, setState);
   }, [setState, checkAndRequestPermissions]);

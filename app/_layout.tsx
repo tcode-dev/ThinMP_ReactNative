c1042f2553131d0225db5c72fb1aa8fc6298e2b4
tcode <tk@tcode.dev>
2025-01-21 08:22:19 +0900

    feat: wrap RootLayout with AppRealmProvider to integrate Realm context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,25 +2,28 @@ import { Stack } from 'expo-router';
 import BackButton from '@/components/molecules/button/BackButton';
 import CustomHeaderBackground from '@/components/molecules/CustomHeaderBackground';
 import Permission from '@/components/organisms/Permission';
+import AppRealmProvider from '@/realm/AppRealmProvider';
 
 export default function RootLayout() {
   return (
-    <Permission>
-      <Stack
-        screenOptions={({ navigation }) => ({
-          title: '',
-          headerTransparent: true,
-          headerLeft: () => <BackButton onPress={() => navigation.goBack()} />,
-        })}
-      >
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="artists/index" options={{ headerBackground: () => <CustomHeaderBackground title="Artists" /> }} />
-        <Stack.Screen name="artists/[id]" />
-        <Stack.Screen name="albums/index" options={{ headerBackground: () => <CustomHeaderBackground title="Albums" /> }} />
-        <Stack.Screen name="albums/[id]" />
-        <Stack.Screen name="songs" options={{ headerBackground: () => <CustomHeaderBackground title="Songs" /> }} />
-        <Stack.Screen name="player" options={{ headerBackground: () => null }} />
-      </Stack>
-    </Permission>
+    <AppRealmProvider>
+      <Permission>
+        <Stack
+          screenOptions={({ navigation }) => ({
+            title: '',
+            headerTransparent: true,
+            headerLeft: () => <BackButton onPress={() => navigation.goBack()} />,
+          })}
+        >
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="artists/index" options={{ headerBackground: () => <CustomHeaderBackground title="Artists" /> }} />
+          <Stack.Screen name="artists/[id]" />
+          <Stack.Screen name="albums/index" options={{ headerBackground: () => <CustomHeaderBackground title="Albums" /> }} />
+          <Stack.Screen name="albums/[id]" />
+          <Stack.Screen name="songs" options={{ headerBackground: () => <CustomHeaderBackground title="Songs" /> }} />
+          <Stack.Screen name="player" options={{ headerBackground: () => null }} />
+        </Stack>
+      </Permission>
+    </AppRealmProvider>
   );
 }

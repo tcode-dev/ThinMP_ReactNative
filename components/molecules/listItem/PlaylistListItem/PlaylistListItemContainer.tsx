979aecca53ec89f295e6ed48bc288c25a96c2514
tcode <tk@tcode.dev>
2025-02-07 07:52:05 +0900

    feat: enhance shortcut functionality in PlaylistListItemContainer

--- components/molecules/listItem/PlaylistListItem/PlaylistListItemContainer.tsx
+++ components/molecules/listItem/PlaylistListItem/PlaylistListItemContainer.tsx
@@ -3,21 +3,22 @@ import { useCallback } from 'react';
 import PlaylistListItemPresenter from './PlaylistListItemPresenter';
 import localize from '@/localize';
 import { PlaylistModel } from '@/model/PlaylistModel';
-import { existsPlaylist } from '@/repository/playlistRepository';
+import { addShortcut, Category, deleteShortcut, existsShortcut } from '@/repository/ShortcutRepository';
 
 const PlaylistListItemContainer: React.FC<PlaylistModel> = (props) => {
+  const playlistId = props.id as unknown as string;
   const href = `/playlists/${props.id}` as Href;
   const router = useRouter();
   const onPress = useCallback(() => {
     router.push(href as Href);
   }, [href, router]);
   const shortcutBuilder = useCallback(() => {
-    if (existsPlaylist(props.id)) {
-      return { label: localize('shortcutRemove'), callback: () => {} };
+    if (existsShortcut(playlistId, Category.Playlist)) {
+      return { label: localize('shortcutRemove'), callback: () => deleteShortcut(playlistId, Category.Playlist) };
     } else {
-      return { label: localize('shortcutAdd'), callback: () => {} };
+      return { label: localize('shortcutAdd'), callback: () => addShortcut(playlistId, Category.Playlist) };
     }
-  }, [props.id]);
+  }, [playlistId]);
   const builders = [shortcutBuilder];
 
   return <PlaylistListItemPresenter {...props} builders={builders} onPress={onPress} />;

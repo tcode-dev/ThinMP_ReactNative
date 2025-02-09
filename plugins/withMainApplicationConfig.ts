826f3100ba09afb421c36529c0b5a999a8224178
tcode <tk@tcode.dev>
2025-02-09 09:16:17 +0900

    feat: enhance MainApplication lifecycle management by implementing ActivityLifecycleCallbacks

--- plugins/withMainApplicationConfig.ts
+++ plugins/withMainApplicationConfig.ts
@@ -4,20 +4,38 @@ const withMainApplicationConfig = (expoConfig) => withMainApplication(expoConfig
   const pattern = /class MainApplication\s*:\s*Application\(\),\s*ReactApplication\s*\{/;
   const replacement = `
 import android.app.Activity
+import android.os.Bundle
 
-class MainApplication : Application(), ReactApplication {
-//  override fun onActivityDestroyed(p0: Activity) {
+class MainApplication : Application(), ReactApplication, Application.ActivityLifecycleCallbacks {
+  override fun onActivityDestroyed(p0: Activity) {
 //    if (!MusicService.isServiceRunning) return
 
 //    MusicPlayer.dispose()
 
-//     val musicServiceIntent = Intent(applicationContext, MusicService::class.java)
+//    val musicServiceIntent = Intent(applicationContext, MusicService::class.java)
 
-//     applicationContext.stopService(musicServiceIntent)
-//  }
+//    applicationContext.stopService(musicServiceIntent)
+  }
+
+  override fun onActivityCreated(p0: Activity, p1: Bundle?) {}
+
+  override fun onActivityStarted(p0: Activity) {}
+
+  override fun onActivityResumed(p0: Activity) {}
+
+  override fun onActivityPaused(p0: Activity) {}
+
+  override fun onActivityStopped(p0: Activity) {}
+
+  override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {}
+`;
+  const config2 = config.modResults.contents.replace(pattern, replacement);
+  const pattern2 = /ApplicationLifecycleDispatcher.onApplicationCreate\(this\)/;
+  const replacement2 = `ApplicationLifecycleDispatcher.onApplicationCreate(this)
+    registerActivityLifecycleCallbacks(this)
 `;
 
-  config.modResults.contents = config.modResults.contents.replace(pattern, replacement);
+  config.modResults.contents = config2.replace(pattern2, replacement2);
   return config
 })
 

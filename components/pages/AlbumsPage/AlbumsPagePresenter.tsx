3c3d9baca72c9495abb2227dad7f7daf444fabf4
tcode <tk@tcode.dev>
2025-02-22 09:07:17 +0900

    refactor: replace EmptyRowListItem with EmptyFooter in AlbumsPagePresenter for consistency

--- components/pages/AlbumsPage/AlbumsPagePresenter.tsx
+++ components/pages/AlbumsPage/AlbumsPagePresenter.tsx
@@ -4,11 +4,11 @@ import EmptyHeader from '@/components/molecules/listItem/EmptyHeader';
 import PageLayout from '@/components/molecules/PageLayout';
 import AlbumList from '@/components/organisms/list/AlbumList';
 import MiniPlayer from '@/components/organisms/MiniPlayer';
-import EmptyRowListItem from '@/components/molecules/listItem/EmptyFooter';
+import EmptyFooter from '@/components/molecules/listItem/EmptyFooter';
 
 const AlbumsPagePresenter = () => (
   <PageLayout>
-    <FlatList data={[<EmptyHeader key={0} />, <AlbumList key={1} />, <EmptyRowListItem key={2} />]} renderItem={({ item }) => <>{item}</>} />
+    <FlatList data={[<EmptyHeader key={0} />, <AlbumList key={1} />, <EmptyFooter key={2} />]} renderItem={({ item }) => <>{item}</>} />
     <MiniPlayer />
   </PageLayout>
 );

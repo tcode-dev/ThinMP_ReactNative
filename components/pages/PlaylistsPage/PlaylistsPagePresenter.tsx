a9eb2a051bdcb9037db4215716dfdc7729c55deb
tcode <tk@tcode.dev>
2025-03-02 07:40:56 +0900

    feat: add PlaylistsHeader component to PlaylistsPagePresenter for improved UI

--- components/pages/PlaylistsPage/PlaylistsPagePresenter.tsx
+++ components/pages/PlaylistsPage/PlaylistsPagePresenter.tsx
@@ -3,9 +3,11 @@ import PageLayout from '@/components/molecules/PageLayout';
 import PlaylistList from '@/components/organisms/list/PlaylistList';
 import MiniPlayer from '@/components/organisms/MiniPlayer';
 import PageScrollableContent from '@/components/molecules/PageScrollableContent';
+import PlaylistsHeader from '@/components/molecules/header/PlaylistsHeader';
 
 const PlaylistsPagePresenter = () => (
   <PageLayout>
+    <PlaylistsHeader />
     <PageScrollableContent data={[<PlaylistList />]} />
     <MiniPlayer />
   </PageLayout>

9e46380cc8c138a424651686f5413d8981384dc0
tcode <tk@tcode.dev>
2025-03-07 08:05:01 +0900

    feat: replace useEffect with useFocusEffect for loading favorite songs

--- components/pages/FavoriteSongsPage/FavoriteSongsPageContainer.tsx
+++ components/pages/FavoriteSongsPage/FavoriteSongsPageContainer.tsx
@@ -1,16 +1,19 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import FavoriteSongsPagePresenter from './FavoriteSongsPagePresenter';
 import { useSongsStore } from '@/store/songsStore';
 import { usePlayer } from '@/hooks/usePlayer';
+import { useFocusEffect } from 'expo-router';
 
 const FavoriteSongsPageContainer = () => {
   const { loadFavoriteSongs } = useSongsStore();
   const { playSongs } = usePlayer();
   const play = useCallback((index: number) => playSongs(index), [playSongs]);
 
-  useEffect(() => {
-    loadFavoriteSongs();
-  }, [loadFavoriteSongs]);
+  useFocusEffect(
+    useCallback(() => {
+      loadFavoriteSongs();
+    }, [loadFavoriteSongs]),
+  );
 
   return <FavoriteSongsPagePresenter play={play} />;
 };

e66a9dcd399e87b1677601f35e1426ecbabc237a
tcode <tk@tcode.dev>
2025-03-15 10:16:41 +0900

    sort sample

--- components/pages/MainEditPage/MainEditPagePresenter.tsx
+++ components/pages/MainEditPage/MainEditPagePresenter.tsx
@@ -3,14 +3,16 @@ import { FlatList } from 'react-native';
 import PageLayout from '@/components/molecules/PageLayout';
 import MainMenu from '@/components/organisms/MainMenu';
 import EmptyFooter from '@/components/molecules/listItem/EmptyFooter';
+import SortableList from '@/components/organisms/list/SortableList';
 
 const MainEditPagePresenter = () => (
   <PageLayout>
-    <FlatList
+    {/* <FlatList
       data={[<MainMenu />, <EmptyFooter />]}
       renderItem={({ item }) => <>{item}</>}
       keyExtractor={(item, index) => index.toString()}
-    />
+    /> */}
+    <SortableList />
   </PageLayout>
 );
 

35383cf7d294e4bb825cd3acf460ba15b63befb8
tcode <tk@tcode.dev>
2025-03-23 08:20:33 +0900

    feat: add MainHeader component with container and presenter structure

--- components/pages/MainPage/MainPagePresenter.tsx
+++ components/pages/MainPage/MainPagePresenter.tsx
@@ -5,7 +5,7 @@ import RecentlyAddedTitle from '@/components/molecules/title/RecentlyAddedTitle'
 import ShortcutTitle from '@/components/molecules/title/ShortcutTitle';
 import AlbumList from '@/components/organisms/list/AlbumList';
 import ShortcutList from '@/components/organisms/list/ShortcutList';
-import MainHeader from '@/components/organisms/main/MainHeader';
+import MainHeader from '@/components/molecules/header/MainHeader';
 import MainMenu from '@/components/organisms/MainMenu';
 import MiniPlayer from '@/components/organisms/MiniPlayer';
 import EmptyFooter from '@/components/molecules/listItem/EmptyFooter';

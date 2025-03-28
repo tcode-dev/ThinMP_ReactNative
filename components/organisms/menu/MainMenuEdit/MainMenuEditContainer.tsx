2a797d50cf32a41b7ee3b6345c5798ff871208ad
tcode <tk@tcode.dev>
2025-03-29 08:34:51 +0900

    feat: enhance MainMenuEdit with theme color support and improve drag-and-drop UI

--- components/organisms/menu/MainMenuEdit/MainMenuEditContainer.tsx
+++ components/organisms/menu/MainMenuEdit/MainMenuEditContainer.tsx
@@ -1,12 +1,14 @@
+import { useThemeColor } from '@/hooks/useThemeColor';
 import MainMenuEditPresenter from './MainMenuEditPresenter';
 import { useMainMenuEditStore } from '@/store/mainMenuEditStore';
 
 const MainMenuEditContainer = () => {
   const { state, toggle, update } = useMainMenuEditStore();
+  const color = useThemeColor();
 
   if (!state.isReady) return;
 
-  return <MainMenuEditPresenter list={state.value} onPress={toggle} onDragEnd={update} />;
+  return <MainMenuEditPresenter list={state.value} borderBottomColor={color.border} iconColor={color.icon} onPress={toggle} onDragEnd={update} />;
 };
 
 export default MainMenuEditContainer;
--- components/organisms/menu/MainMenuEdit/MainMenuEditPresenter.tsx
+++ components/organisms/menu/MainMenuEdit/MainMenuEditPresenter.tsx
@@ -1,28 +1,50 @@
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View, StyleSheet } from 'react-native';
 import { MainMenuModel } from '@/model/MainMenuModel';
 import CheckBoxListItem from '@/components/molecules/listItem/CheckBoxListItem';
 import DraggableFlatList from 'react-native-draggable-flatlist';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 type Props = {
   list: MainMenuModel[],
+  borderBottomColor: string,
+  iconColor: string,
   onPress: (item: MainMenuModel) => void,
   onDragEnd: (data: MainMenuModel[]) => void,
 };
 
-const MainMenuEditPresenter: React.FC<Props> = ({ list, onPress, onDragEnd }) => (
+const MainMenuEditPresenter: React.FC<Props> = ({ list, borderBottomColor, iconColor, onPress, onDragEnd }) => (
   <GestureHandlerRootView>
     <DraggableFlatList
       data={list}
       renderItem={({ item, drag }) => (
-        <TouchableOpacity onLongPress={drag}>
-          <CheckBoxListItem isChecked={item.visibility} onPress={() => onPress(item)}>{item.text}</CheckBoxListItem>
-        </TouchableOpacity>
+        <View style={styles.listItem}>
+          <CheckBoxListItem isChecked={item.visibility} onPress={() => onPress(item)}>
+            {item.text}
+          </CheckBoxListItem>
+          <View style={[styles.drag, { borderBottomColor }]}>
+            <TouchableOpacity onPressIn={drag}>
+              <MaterialIcons name="drag-handle" size={24} color={iconColor} />
+            </TouchableOpacity>
+          </View>
+        </View>
       )}
       keyExtractor={(item) => item.item.toString()}
       onDragEnd={({ data }) => onDragEnd(data)}
     />
   </GestureHandlerRootView>
 );
+
+const styles = StyleSheet.create({
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  drag: {
+    padding: 10,
+    borderBottomWidth: 1,
+    height: '100%',
+  },
+});
 
 export default MainMenuEditPresenter;

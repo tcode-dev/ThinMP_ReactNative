004bad7220192a3717bd17b613ee4850653b0ec6
tcode <tk@tcode.dev>
2025-03-31 08:43:57 +0900

    feat: update MainEditHeaderContainer to save visibility state on done

--- components/molecules/header/MainEditHeader/MainEditHeaderContainer.tsx
+++ components/molecules/header/MainEditHeader/MainEditHeaderContainer.tsx
@@ -1,10 +1,18 @@
+import { useVisibilityStore } from '@/store/visibilityStore';
 import EditHeader from '../EditHeader';
 import { useMainMenuEditStore } from '@/store/mainMenuEditStore';
+import { useCallback } from 'react';
 
 const MainEditHeaderContainer = () => {
   const { saveMainMenu } = useMainMenuEditStore();
+  const { saveVisibility } = useVisibilityStore();
+  const done = useCallback(() => {
+    saveVisibility();
+    saveMainMenu();
+  }
+  , [saveVisibility, saveMainMenu]);
 
-  return <EditHeader done={saveMainMenu}  />;
+  return <EditHeader done={done}  />;
 };
 
 export default MainEditHeaderContainer;

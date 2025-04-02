fb39768c9ec5669b4843b061486d3ee7e73a7271
tcode <tk@tcode.dev>
2025-04-03 08:33:22 +0900

    fix: correct localization key for favorite songs header

--- components/molecules/header/FavoriteSongsHeader/FavoriteSongsHeaderContainer.tsx
+++ components/molecules/header/FavoriteSongsHeader/FavoriteSongsHeaderContainer.tsx
@@ -3,7 +3,7 @@ import Header from '../Header';
 import FavoriteSongsMenuButton from '@/components/molecules/button/menu/FavoriteSongsMenuButton';
 
 const FavoriteSongsHeaderContainer = () => {
-  const title = localize('favoriteSong');
+  const title = localize('favoriteSongs');
 
   return <Header title={title} menu={<FavoriteSongsMenuButton />} />;
 };

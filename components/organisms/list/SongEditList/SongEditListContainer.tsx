3b965b348ecb77d4c8e5ff4e9f1eb414e5136ccf
tcode <tk@tcode.dev>
2025-04-05 09:13:09 +0900

    feat: update SongEditListContainer to manage song list state with useEffect

--- components/organisms/list/SongEditList/SongEditListContainer.tsx
+++ components/organisms/list/SongEditList/SongEditListContainer.tsx
@@ -1,12 +1,19 @@
+import { useEffect, useState } from 'react';
 import SongEditListPresenter from './SongEditListPresenter';
 import { useSongsStore } from '@/store/songsStore';
+import { SongModel } from '@/model/SongModel';
 
 const SongEditListContainer = () => {
   const { state, update } = useSongsStore();
+  const [list, setList] = useState<SongModel[]>([]);
 
-  if (!state.isReady) return null;
+  useEffect(() => {
+    if (state.isReady) {
+      setList(state.value);
+    }
+  }, [state]);
 
-  return <SongEditListPresenter songs={state.value} onDragEnd={update} />;
+  return <SongEditListPresenter songs={list} onDragEnd={update} />;
 };
 
 export default SongEditListContainer;
